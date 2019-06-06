05a4630237b863a7f5bec9bc1b452d4e970e6fcd
JHompesc-dev <jhompesc@avans.nl>
2019-06-06 12:35:11 +0200

    Backend #9 eventhandlers
    eventhandlers and auth cfg

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const NodeMediaServer = require('node-media-server');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+var crypto = require('crypto');
 
 const config = {
     rtmp: {
@@ -21,8 +22,70 @@ const config = {
             mp4: true,
             mp4Flags: '[movflags=faststart]',
         }]
+    },
+    auth: {
+      play: true,
+      publish: true,
+      secret: 'nodemedia2017privatekey'
     }
 };
 
 var nms = new NodeMediaServer(config)
 nms.run();
+
+nms.on('preConnect', (id, args) => {
+    //console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
+    // let session = nms.getSession(id);
+    // session.reject();
+  });
+  
+  nms.on('postConnect', (id, args) => {
+    //console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
+  });
+  
+  nms.on('doneConnect', (id, args) => {
+    //console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
+  });
+
+ 
+
+  
+  nms.on('prePublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+
+    let S2 = crypto.createHash('md5').update(StreamPath).digest("hex");
+
+    //StreamPath = S2;
+
+    //nms.StreamPath = S2;
+
+    console.log('Streampath: ', nms.StreamPath);
+
+
+    // let session = nms.getSession(id);
+    // session.reject();
+  });
+  
+  nms.on('postPublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  });
+  
+  nms.on('donePublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  });
+
+  //event handlers
+
+  nms.on('prePlay', (id, StreamPath, args) => {
+    console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    // let session = nms.getSession(id);
+    // session.reject();
+  });
+  
+  nms.on('postPlay', (id, StreamPath, args) => {
+    console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  });
+  
+  nms.on('donePlay', (id, StreamPath, args) => {
+    console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  });
