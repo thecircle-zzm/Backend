431b654669f06b525589067dc4b6f43dcd11be81
Marco van Poortvliet <Marc0tjevp@gmail.com>
2019-06-12 12:43:37 +0200

    Code cleanup

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const userSM = require('../models/schemas/user.schema.js')
-var crypto = require('crypto');
+var crypto = require('crypto')
 const lUser = userSM.userModel
 
 
@@ -10,73 +10,66 @@ function createUser(req, res) {
     let emailNew = req.body.email || ''
     let passwordNew = req.body.password || ''
     let publicKeyNew = req.body.publicKey || ''
-    let hashNew;
-    let saltNew;
+    let hashNew
+    let saltNew
 
+    lUser.findOne({
+            username: usernameNew
+        })
+        .then((luser) => {
+            if (luser === null) {
 
-    lUser.findOne ({ username: usernameNew }) 
-     .then((luser) =>{
-        if(luser === null)
-        {
-            
-
-
-            var genRandomString = function(length){
-                return crypto.randomBytes(Math.ceil(length/2))
+                let genRandomString = function (length) {
+                    return crypto.randomBytes(Math.ceil(length / 2))
                         .toString('hex') /** convert to hex */
-                        .slice(0,length);   /** return required num of char */
-            };
-
-            var sha512 = function(password, salt){
-                var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-                hash.update(password);
-                var value = hash.digest('hex');
-                return {
-                    salt:salt,
-                    passwordHash:value
-                };
-            };
-
-            function saltHashPassword(userpassword) {
-                var salt = genRandomString(16); /** Salt length 16 or var */
-                var passwordData = sha512(userpassword, salt);
-                console.log('UserPassword = '+userpassword);
-                console.log('Passwordhash = '+passwordData.passwordHash);
-                hashNew = passwordData.passwordHash;
-                saltNew = passwordData.salt;
-                console.log('nSalt = '+passwordData.salt);
-            }
-            
-            saltHashPassword(passwordNew);
-
-
-            let newUser = new lUser({
-                username: usernameNew,
-                email: emailNew,
-                passwordHash: hashNew,
-                passwordSalt: saltNew,
-                publicKey: publicKeyNew
-            })
-
-            newUser.save(function (err) {
-                if (err) {
-                    res.status(500).json(err).end()
-                } else {
-                    res.status(200).send()
+                        .slice(0, length) /** return required num of char */
                 }
-            })
 
-        } else {
-            res.status(403).send({Error: "User already exists!"})
-            session.reject();
-        }
+                let sha512 = function (password, salt) {
+                    let hash = crypto.createHmac('sha512', salt) /** Hashing algorithm sha512 */
+                    hash.update(password)
+                    let value = hash.digest('hex')
+                    return {
+                        salt: salt,
+                        passwordHash: value
+                    }
+                }
 
-     })
+                function saltHashPassword(userpassword) {
+                    let salt = genRandomString(16) /** Salt length 16 or var */
+                    let passwordData = sha512(userpassword, salt)
+                    hashNew = passwordData.passwordHash
+                    saltNew = passwordData.salt
+                }
+
+                saltHashPassword(passwordNew)
+
+                let newUser = new lUser({
+                    username: usernameNew,
+                    email: emailNew,
+                    passwordHash: hashNew,
+                    passwordSalt: saltNew,
+                    publicKey: publicKeyNew
+                })
+
+                newUser.save(function (err) {
+                    if (err) {
+                        res.status(500).json(err).end()
+                    } else {
+                        res.status(200).send()
+                    }
+                })
+
+            } else {
+                res.status(403).send({
+                    Error: "User already exists!"
+                })
+                session.reject()
+            }
+
+        })
+
 }
-
-    
-
-
 
 module.exports = {
     createUser
--- utils/streaming.util.js
+++ utils/streaming.util.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose')
 const userSM = require('../models/schemas/user.schema.js')
 const lUser = userSM.userModel
 
-
 const config = {
     rtmp: {
         port: 1935,
@@ -33,64 +32,63 @@ const config = {
         api_user: 'zzm',
         api_pass: 'zzm123'
     }
-};
+}
 
 var nms = new NodeMediaServer(config)
-nms.run();
+nms.run()
 
 nms.on('preConnect', (id, args) => {
-    console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-     let session = nms.getSession(id);
+    console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`)
+    let session = nms.getSession(id)
 
-     const loginUser = 'test';
-     const loginPassword = 'password';
+    const loginUser = 'test'
+    const loginPassword = 'password'
 
-     lUser.findOne ({ userName: loginUser }) 
-     .then((luser) =>{
-        if(luser === null)
-        {
-            res.status(403).send({Error: "User does not exist!"})
-            session.reject();
-        }
-        else{
+    lUser.findOne({
+            userName: loginUser
+        })
+        .then((luser) => {
+            if (luser === null) {
+                res.status(403).send({
+                    Error: "User does not exist!"
+                })
+                session.reject()
+            } else {
 
-            var sha512 = function(password, salt){
-                var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-                hash.update(password);
-                var value = hash.digest('hex');
-                return {
-                    salt:salt,
-                    passwordHash:value
-                };
-            };
-
-            function saltHashPassword(userpassword) {
-                var salt = luser.passwordSalt;
-                var passwordData = sha512(userpassword, salt);
-                console.log('UserPassword = '+userpassword);
-                console.log('Passwordhash = '+passwordData.passwordHash);
-                console.log('nSalt = '+passwordData.salt);
-
-                if (passwordData.passwordHash == luser.passwordHash){
-                    console.log('Passwords match');
-                }else {
-                    res.status(401).send({Error: "Login error"})
-                    session.reject();
+                var sha512 = function (password, salt) {
+                    var hash = crypto.createHmac('sha512', salt) /** Hashing algorithm sha512 */
+                    hash.update(password)
+                    var value = hash.digest('hex')
+                    return {
+                        salt: salt,
+                        passwordHash: value
+                    }
                 }
 
+                function saltHashPassword(userpassword) {
+                    var salt = luser.passwordSalt
+                    var passwordData = sha512(userpassword, salt)
+                    console.log('UserPassword = ' + userpassword)
+                    console.log('Passwordhash = ' + passwordData.passwordHash)
+                    console.log('nSalt = ' + passwordData.salt)
+
+                    if (passwordData.passwordHash == luser.passwordHash) {
+                        console.log('Passwords match')
+                    } else {
+                        res.status(401).send({
+                            Error: "Login error"
+                        })
+                        session.reject()
+                    }
+                }
+                saltHashPassword(loginPassword)
             }
-
-
-            saltHashPassword(loginPassword);
-            
-        }
-    })
-    .catch(next)
-
-});
+        })
+        .catch(next)
+})
 
 nms.on('prePublish', (id, StreamPath, args) => {
     let session = nms.getSession(id)
     let s2 = session.publishStreamPath
     session.publishStreamPath = '/live/' + crypto.createHash('sha256').update(s2).digest("hex")
-});
+})
