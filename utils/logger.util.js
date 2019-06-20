88e97480c275a5e56855d749769d9849285cb612
Marco van Poortvliet <Marc0tjevp@gmail.com>
2019-06-20 09:49:38 +0200

    Added more tag emojis

--- utils/logger.util.js
+++ utils/logger.util.js
@@ -1,12 +1,14 @@
 let log = (tag, message) => {
 
-    let emoji = 'X'
+    let emoji = '❕'
 
     if (tag == 'chat') emoji = '💬'
     if (tag == 'stream') emoji = '🎬'
     if (tag == 'collection') emoji = '💾'
     if (tag == 'api') emoji = '⚙️ '
     if (tag == 'token') emoji = '💰'
+    if (tag == 'screenshot') emoji = '🖼️'
+    if (tag == 'error') emoji = '🛑'
 
     console.log(emoji + " [" + tag.toUpperCase() + "] - " + message)
 }
